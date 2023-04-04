68a87538d738bdc749b94ccedfaace0493add0cf
BRIAN BETT <+>
2023-04-04 12:00:19 +0300

    Completed Assignment

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # phase-1-week-3-code-challenge
+**<FLATDANGO>** 
+<project description>
+
+This is an application, Flatdango, that allows a user to purchase movie tickets from the 
+theater.
+
+## REQUIREMENTS
+Use this JSON file for your server db.json
+'https://localhost:3000/films'
+
+### Prerequisites
+* npm install -g json-server
+* json-server --watch db.json
+* Javascipt
+* CSS
+* HTML
+
+Meta
+----
+Author:
+   * **Brian Kiprono Bett ** - *Initial work* - [brianbett1998](<https://github.com/brianbett1998>)
+Status:
+    maintained, and is currently in development
+Version:
+    v0.1.0
+Usage
+-----
+## License
+This project is licensed under the MIT License
+Documentation
+-------------
---
+++ index.js
@@ -0,0 +1,60 @@
+const listHolder = document.getElementById('films');
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementsByClassName('film item')[0].remove();
+    fetchMovies('http://localhost:3000/films');
+});
+
+function fetchMovies(apiUrl) {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(movies => {
+        movies.forEach(movie => {
+            displayMovie(movie);
+        });
+    });
+}
+
+function displayMovie(movie) {
+    const list = document.createElement('li');
+    list.style.cursor = "cell";
+    list.textContent = movie.title;
+    listHolder.appendChild(list);
+    addClickEvent(list, movie.id);
+}
+
+function addClickEvent(element, id) {
+    element.addEventListener('click', () => {
+        fetch(`http://localhost:3000/films/${id}`)
+        .then(response => response.json())
+        .then(movie => {
+            document.getElementById('buy-ticket').textContent = 'Buy Ticket';
+            setUpMovieDetails(movie);
+        });
+    });
+}
+
+function setUpMovieDetails(funMovie) {
+    const preview = document.getElementById('poster');
+    preview.src = funMovie.poster;
+    const movieTitle = document.querySelector('#title');
+    movieTitle.textContent = funMovie.title;
+    const movieTime = document.querySelector('#runtime');
+    movieTime.textContent = `${funMovie.runtime} minutes`;
+    const movieDescription = document.querySelector('#film-info');
+    movieDescription.textContent = funMovie.description;
+    const showTime = document.querySelector('#showtime');
+    showTime.textContent = funMovie.showtime;
+    const tickets = document.querySelector('#ticket-number');
+    tickets.textContent = funMovie.capacity - funMovie.tickets_sold;
+}
+
+const btn = document.getElementById('buy-ticket');
+btn.addEventListener('click', function(event){
+    let remainingTickets = document.querySelector('#ticket-number').textContent;
+    event.preventDefault();
+    if (remainingTickets > 0) {
+        document.querySelector('#ticket-number').textContent = remainingTickets - 1;
+    } else if (parseInt(remainingTickets, 10) === 0) {
+        btn.textContent = 'Sold Out';
+    }
+});
